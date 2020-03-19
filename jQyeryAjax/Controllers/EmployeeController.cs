d7bbccc50afd75103269cc4310868157f0af2c0c
hashemhassan99 <hashem25799@gmail.com>
2020-03-19 18:52:29 +0200

    Add project files.

---
+++ jQyeryAjax/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using jQyeryAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQyeryAjax.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (DBModel db= new DBModel())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+       public ActionResult AddorEdit(int id=0)
+        {
+            Employee emp = new Employee();  
+            //update function or edit function
+            if(id !=0)
+            {
+                using (DBModel db= new DBModel())
+                {
+                    emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                }
+            }
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(Employee emp)
+        {
+            try {
+                //check if emp has an image or not
+                if (emp.ImageUpload != null)
+                {
+                    String fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    String extention = Path.GetExtension(emp.ImageUpload.FileName);
+                    //i will create custom file name to avoid repetiation in folder file name
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extention;
+                    emp.ImagePath = "~/AppFiles/Images" + fileName;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images"), fileName));
+                }
+                using (DBModel db = new DBModel())
+                {
+                    //now for edit
+                    if (emp.EmployeeID == 0)
+                    {
+                        db.Employees.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                   
+                }
+                //here i will return json object to print the sucsses mesage or the fail message
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Submited Successfily" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false,  message = ex.Message}, JsonRequestBehavior.AllowGet);
+            }
+           
+        }   
+        public ActionResult Delet(int id)
+        {
+            try
+            {
+                using(DBModel db= new DBModel())
+                {
+                    Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                    db.Employees.Remove(emp);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Deleted Successfily" }, JsonRequestBehavior.AllowGet);
+            }
+            catch(Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
---
+++ jQyeryAjax/Scripts/Scripts.js
@@ -0,0 +1,101 @@
+﻿$(function () {
+    $('#loaderbody').addClass('hide');
+});
+
+function ShowImagePreivew(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imagePreview').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imgInp").change(function () {
+    ShowImagePreivew(this);
+});
+
+
+//this function can be used for any form with file input or not
+function jQueryAjaxPost (form) {
+    $.validator.unobtrusive.parse(form);    
+    if ($(form).valid()) {
+        var ajaxconfig = {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                if (response.success) {
+                    $("#firsttabe").html(response.html);
+                    refreshAddNewTab($(form).attr('data-resetUrl'), true);
+                    //success message
+                    $.notify(response.message, "success");
+                    if (typeof activatejQuerytable !== 'undefined' && $.isFunction(activatejQuerytable))
+                        activatejQuerytable();
+                }
+                else {
+
+                    //error message
+                    $.notify(response.message, "error")
+                }
+            }
+
+        }
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxconfig["contentType"] = false;
+            ajaxconfig["processData"] = false;
+        }
+        $.ajax(ajaxconfig);
+
+    }
+    return false;
+}
+
+//function to refresh the form and redierct to view all tab
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $("#secondtabe").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');//index 1 to Add new tab
+            if (showViewTab)
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');//index 0 to View All tab  
+        }
+     });
+}  
+//function for edit 
+function Edit(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            $("#secondtabe").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');//index 1 to Add new tab
+                $('ul.nav.nav-tabs a:eq(1)').tab('show');//index 0 to View All tab  
+        }
+    }); 
+}
+//function for to delet
+function Delet(url) {
+    if (confirm('Are you sure to delet this record?')) {
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#firsttabe").html(response.html);
+                    $.notify(response.message, "warn");
+                    if (typeof activatejQuerytable !== 'undefined' && $.isFunction(activatejQuerytable))
+                        activatejQuerytable();
+                } else {
+                    $.notify(response.message, "error"); 
+                }
+            }
+        }); 
+
+    }
+}
